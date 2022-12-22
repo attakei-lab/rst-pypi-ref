2e2f11f0ffdd32e98786c66f73bb48ca0808616d
Kazuya Takei <myself@attakei.net>
2022-12-22 23:41:06 +0900

    refactor: Add test

--- tests/test_it.py
+++ tests/test_it.py
@@ -1,2 +1,10 @@
-def test_it():
-    assert True
+from rst_pypi_ref import core
+
+
+def test_pypi_reference_role():
+    nodes, messages = core.pypi_reference_role(
+        "pypi", ":pypi:`docutils`", "docutils", 0, None
+    )
+    assert len(nodes) == 1
+    node = nodes[0]
+    assert node["refuri"] == "https://pypi.org/project/docutils"
