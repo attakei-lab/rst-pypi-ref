700779e53a254d036fdcf7524763d3ae07f61383
Kazuya Takei <myself@attakei.net>
2024-02-18 10:50:20 +0900

    ci: Use setup/teardown

--- tests/test_it.py
+++ tests/test_it.py
@@ -34,19 +34,21 @@ class TestForBuildPackageUrl:
 
 
 class TestForParse:
-    def test_simple_name_only(self):
-        from docutils import nodes
-
+    def setup_method(self):
         core.bootstrap()
 
+    def teardown_method(self):
+        from docutils.parsers.rst import roles
+
+        del roles._role_registry["pypi"]
+
+    def test_simple_name_only(self):
         doctree: nodes.document = publish_doctree(":pypi:`docutils`")
         refs = list(doctree.findall(nodes.reference))
         assert len(refs) == 1
         assert refs[0]["refuri"] == "https://pypi.org/project/docutils/"
 
     def test_simple_with_version(self):
-        core.bootstrap()
-
         doctree: nodes.document = publish_doctree(":pypi:`docutils==0.3`")
         refs = list(doctree.findall(nodes.reference))
         assert len(refs) == 1
