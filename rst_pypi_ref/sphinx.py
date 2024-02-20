f9aca8dfb21c3384f95c3363080e104a1fc22bc4
Kazuya Takei <myself@attakei.net>
2024-02-21 02:39:41 +0900

    feat: Integrate for sphinx

--- rst_pypi_ref/sphinx.py
+++ rst_pypi_ref/sphinx.py
@@ -7,12 +7,23 @@ from .core import VerifyOptions, configure
 
 
 def register_roles(app: Sphinx, config: Config):
-    options = VerifyOptions()
+    options = config.rst_pypi_ref_options
+    if options is None:
+        options = VerifyOptions()
+    elif isinstance(options, VerifyOptions):
+        pass
+    elif isinstance(options, dict):
+        strict_version = bool(options.get("strict_version", False))
+        ref_pypi_site = bool(options.get("ref_pypi_site", False))
+        options = VerifyOptions(
+            strict_version=strict_version, ref_pypi_site=ref_pypi_site
+        )
     configure(options)
 
 
 def setup(app: Sphinx):
     app.connect("config-inited", register_roles)
+    app.add_config_value("rst_pypi_ref_options", None, "env", [dict, VerifyOptions])
     return {
         "version": __version__,
         "env_version": 1,
--- tests/test_sphinx.py
+++ tests/test_sphinx.py
@@ -2,9 +2,57 @@
 import pytest
 from sphinx.testing.util import SphinxTestApp
 
+from rst_pypi_ref.core import VerifyOptions
+
 
 @pytest.mark.sphinx("html", testroot="default")
 def test_work_on_sphinx(app: SphinxTestApp):
+    app.build()
+    index_html = app.outdir / "index.html"
+    assert index_html.exists()
+    assert "https://pypi.org/project/sphinx-revealjs/" in index_html.read_text()
+
+
+@pytest.mark.sphinx(
+    "html", testroot="default", confoverrides={"rst_pypi_ref_options": {}}
+)
+def test_work_with_empty_options(app: SphinxTestApp):
+    app.build()
+    index_html = app.outdir / "index.html"
+    assert index_html.exists()
+    assert "https://pypi.org/project/sphinx-revealjs/" in index_html.read_text()
+
+
+@pytest.mark.sphinx(
+    "html",
+    testroot="default",
+    confoverrides={"rst_pypi_ref_options": {"strict_version": True}},
+)
+def test_work_with_strict_version(app: SphinxTestApp):
+    app.build()
+    index_html = app.outdir / "index.html"
+    assert index_html.exists()
+    assert "https://pypi.org/project/sphinx-revealjs/" in index_html.read_text()
+
+
+@pytest.mark.sphinx(
+    "html",
+    testroot="default",
+    confoverrides={"rst_pypi_ref_options": {"ref_pypi_site": True}},
+)
+def test_work_with_ref_pypi_site(app: SphinxTestApp):
+    app.build()
+    index_html = app.outdir / "index.html"
+    assert index_html.exists()
+    assert "https://pypi.org/project/sphinx-revealjs/" in index_html.read_text()
+
+
+@pytest.mark.sphinx(
+    "html",
+    testroot="default",
+    confoverrides={"rst_pypi_ref_options": {"ref_pypi_site": VerifyOptions()}},
+)
+def test_work_with_instance(app: SphinxTestApp):
     app.build()
     index_html = app.outdir / "index.html"
     assert index_html.exists()
