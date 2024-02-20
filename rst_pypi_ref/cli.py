862ad1f07f49ff04d2942014ead4b308552c2d80
Kazuya Takei <myself@attakei.net>
2024-02-20 23:34:53 +0900

    fix: Split arguments for rst-pypi-ref and docutils

--- rst_pypi_ref/cli.py
+++ rst_pypi_ref/cli.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 """Local work-check endpoint."""
+import argparse
+import sys
+
 from docutils.core import publish_cmdline
 
 from rst_pypi_ref.core import VerifyOptions, configure
 
+parser = argparse.ArgumentParser(allow_abbrev=False)
+parser.add_argument("--strict-version", action="store_true")
+parser.add_argument("--ref-pypi-site", action="store_true")
+
+
 if __name__ == "__main__":
-    options = VerifyOptions()
+    argv = []
+    if "--strict-version" in sys.argv:
+        sys.argv.remove("--strict-version")
+        argv.append("--strict-version")
+    if "--ref-pypi-site" in sys.argv:
+        sys.argv.remove("--ref-pypi-site")
+        argv.append("--ref-pypi-site")
+    args = parser.parse_args(argv)
+    options = VerifyOptions(
+        strict_version=args.strict_version, ref_pypi_site=args.ref_pypi_site
+    )
     configure(options)
     publish_cmdline()
